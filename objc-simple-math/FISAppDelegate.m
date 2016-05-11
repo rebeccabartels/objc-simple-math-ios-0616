4d2132c6697493b7ebb8fc0ff8ec6cc9d67b4a96
rebeccabartels <rebeccakbartels@icloud.com>
2016-05-11 17:02:57 -0400

    Done.

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -9,7 +9,34 @@
     
     /**
      
-     * Write your code here.
+     *
+     NSInteger i=0;
+     NSLog(@"i: %li", i);
+     i=1+1;
+     NSLog(@"i: %li", i);
+     NSInteger i = 5 + 8
+     NSLog(@"i: %li", i);
+     
+     NSInteger a = 0;
+     
+     NSInteger b = 0;
+     
+     NSInteger c = a + b;
+     NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+     
+     NSInteger a = 17;
+     NSInteger b = 29;
+     NSInteger c = a + b;
+     
+     NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+     
+     NSUInteger u = 1;
+     NSLog(@"u: %lu", u);
+     u = 2 + 3;
+     NSLog(@"u: %lu", u);
+     NSInteger u = -1;
+     
+     
      
      */
     
